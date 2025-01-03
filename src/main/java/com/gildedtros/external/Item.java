ea253a02e4c7607ed5883de43b4d226f265427ba
Bram De Cneudt <bram@icodeit.be>
2025-01-03 21:19:14 +0100

    GT-1 - create testcases for existing code
    - added testcases for existing code so refactoring so easier
    - moved item class to external package because item class code cannot be changed
    - adding test for item class

--- src/main/java/com/gildedtros/external/Item.java
+++ src/main/java/com/gildedtros/external/Item.java
@@ -1,4 +1,4 @@
-package com.gildedtros;
+package com.gildedtros.external;
 
 public class Item {
 
---
+++ src/test/java/com/gildedtros/external/ItemTest.java
@@ -0,0 +1,14 @@
+package com.gildedtros.external;
+
+import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    void test() {
+    }
+
+}
